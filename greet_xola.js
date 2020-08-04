4431b9bcd55d3a67615575ace0e63598e0afe27f
Zola Magama <zolamagama1985@gmail.com>
2020-08-04 14:54:51 +0200

    initial commit

--- greet_xola.js
+++ greet_xola.js
@@ -1,10 +1,10 @@
 var figlet = require('figlet');
- 
+var chalk = require('chalk');
 figlet('Hello Zola!!', function(err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
         return;
     }
-    console.log(data)
-});
+const styledMessage = chalk.bgGreen.black(data);
+console.log(styledMessage)});
